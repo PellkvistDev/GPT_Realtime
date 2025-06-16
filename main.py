c12b070ec251cc7f61301e8048aebce517c41a7f
PellkvistDev <theo@hihihi.se>
2025-06-16 11:00:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import base64
 import asyncio
 import websockets
 from fastapi import FastAPI, WebSocket, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse
 from dotenv import load_dotenv
 from twilio.twiml.voice_response import VoiceResponse, Connect, Say, Stream
 from fastapi.websockets import WebSocketDisconnect
